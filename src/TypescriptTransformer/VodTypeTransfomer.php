b4f86b22e93966956615d127c3553cb5520a3fe4
deanmcpherson <deanmcpherson@users.noreply.github.com>
2024-09-03 01:28:55 +0000

    Fix styling

--- src/TypescriptTransformer/VodTypeTransfomer.php
+++ src/TypescriptTransformer/VodTypeTransfomer.php
@@ -3,11 +3,8 @@
 namespace Vod\Vod\TypescriptTransformer;
 
 use ReflectionClass;
-use Spatie\TypeScriptTransformer\Structures\MissingSymbolsCollection;
 use Spatie\TypeScriptTransformer\Structures\TransformedType;
 use Spatie\TypeScriptTransformer\Transformers\Transformer;
-use Spatie\TypeScriptTransformer\TypeReflectors\ClassTypeReflector;
-use Vod\Vod\Vod;
 
 class VodTypeTransfomer implements Transformer
 {
